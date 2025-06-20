7c33ae532b7a1cf28abcf3485c33c0714b632798
deepak-07-dk <deepak1652003@gmail.com>
2025-06-20 14:01:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
+var number = prompt("enter your number:");
 let sum=0;
-let number=123;
 let temper=number;
 while(temper>0){
 	let remb=(temper%10);
 sum+=remb*remb*remb;
-temper=(temper/10);
+temper=Math.floor(temper/10);
 }
 if(sum==number){
     console.log("it is armstrong number");
 }else{
 	console.log("it is not armstrong number");
 }	
+
+
+
 
 
 
